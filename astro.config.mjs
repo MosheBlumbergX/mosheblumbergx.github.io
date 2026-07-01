949b0ae617c2c9b4cabe2ead8d1441baa4e8aa7a
Moshe <mosheb@hotmail.co.uk>
2026-07-01 15:12:14 +0000

    Fix: Add base path for project site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mosheb.github.io',
+  site: 'https://mosheblumbergx.github.io',
+  base: '/mosheb.github.io',
   integrations: [mdx()],
 });
